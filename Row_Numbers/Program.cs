6f49ed38900a901cdaa39cdaf147fbd0cd3a8884
Abvorobey <abvorobey@gmail.com>
2023-03-13 13:16:49 +0300

    Ряд чисел рекурсией

---
+++ Row_Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задайте M и N, напишите ряд чисел от M до N
+*/
+using System;
+using static System.Console;
+
+WriteLine("Введите m:");
+int m = int.Parse(ReadLine());
+WriteLine("Введите n:");
+int n = int.Parse(ReadLine());
+string row = PrintRowNumbers (m, n);
+string PrintRowNumbers (int m, int n)
+{
+    if (n == m)
+    {
+        Write($"{m}");
+    }
+    else Write($"{PrintRowNumbers (m, n-1)} {n}");
+    return string.Empty;
+}
